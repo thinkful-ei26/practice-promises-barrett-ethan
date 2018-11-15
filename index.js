c0affddbf66a7000a3c3f4ce0b20028f8cda120c
carpenter-js <carpenterbarrett@gmail.com>
2018-11-15 10:47:06 -0700

    lunch

--- index.js
+++ index.js
@@ -3,36 +3,45 @@ const searchTerm = process.argv[2];
 
 const axios = require('axios');
 
-const BASE_URL = `https://swapi.co/api/people/?search=${searchTerm}`;
+const BASE_URL = 'https://swapi.co/api';
 
 // ${searchTerm}
-function getName(searchTerm){
-  axios.get(`${BASE_URL}`)
+// function getName(searchTerm){
+//   axios.get(`${BASE_URL}`)
+//     .then(response => {
+//       console.log(`${response.data.results[0].name} has been found`);
+//     })
+//     .then(response => {
+//       console.log(`${response.data.results[0].gender} has starred in the following films:`);
+//     })
+//   // .then(response => {
+//   //   console.
+//   // })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
+
+function getNameAndGender(){
+  axios.get(`${BASE_URL}/people/?search=${searchTerm}`)
     .then(response => {
-      console.log(`${response.data.results[0].name} has been found`);
+      console.log(`${response.data.results[0].name} has been found
+    ${response.data.results[0].gender} has starred in the following films:`);
+      return response;
     })
-    .then(response => {
-      console.log(`${response.data.results[0].gender} has starred in the following films:`);
-    })
-  // .then(response => {
-  //   console.
-  // })
-    .catch(error => {
-      console.log(error);
+    .then((response) => {
+      response.data.results[0].films
+        .map(film => axios.get(film))
+        .then(response => {
+          console.log(response);
+        });
     });
 }
 
-function getName(){
-  axios.get(`${BASE_URL}`)
-  .then(response => {
-    console.log(`${response.data.results[0].name} has been found`);
-};
+getNameAndGender(searchTerm);
 
-
-function getGender (){
-  axios.get(`${BASE_URL}`)
-  .then(response => {
-    console.log(`${response.data.results[0].name} has been found`);
-};
-
-function getFilms
+// function getFilms() {
+//     axios.get(`${BASE_URL}/films/`)
+//   .then(response => {
+//     console.log(`${response.data.results[0].gender} has been found`);
+// });
